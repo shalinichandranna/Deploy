731afdee14816e549bdeea68fd2e642ec3bef348
ashleshcoding <ashleshv2004@gmail.com>
2025-04-09 23:30:48 +0530

    Updated API base URLs to use Render backend

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -40,7 +40,7 @@ export const CreateRecipe = () => {
     event.preventDefault();
     try {
       await axios.post(
-        "http://localhost:3001/recipes",
+        "https://recipe-share-8ov7.onrender.com/recipes",
         { ...recipe },
         {
           headers: { authorization: cookies.access_token },
